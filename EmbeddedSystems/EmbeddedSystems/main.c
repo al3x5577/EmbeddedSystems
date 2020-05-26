3891af47aafe8a70041783e5176dffe963d3c695
Alex <alexander.modellbau@gmx.de>
2020-05-26 17:38:01 +0200

    Test gpio functions

--- EmbeddedSystems/EmbeddedSystems/main.c
+++ EmbeddedSystems/EmbeddedSystems/main.c
@@ -9,58 +9,75 @@
 #define F_CPU 1000000
 #include <util/delay.h>
 
+#include <gpio/taster.h>
+#include <gpio/led.h>
+
 
 int main(void) {
 	
-	DDRB = 0xFF;
+    Taster_init();
+    Led_init();
 	
-	
-    /* Replace with your application code */
     while (1) {
-		
-		PORTB = 0xAC;
-		
-		//
-		// Uebung
-		//
-		
-		// Bit 1 in Register PORTB setzten
-		PORTB |= (1 << 1);
-		
-		// Bit 3 in Register PORTB setzten
-		PORTB |= (1 << 3);
-		
-		// Bit 5 in Register PORTB setzten
-		PORTB |= (1 << 5);
-		
-		// Bit 2 in Register PORTB löschen
-		PORTB &= ~(1 << 2);
-		
-		// Bit 4 in Register PORTB löschen
-		PORTB &= ~(1 << 4);
-		
-		// Bit 6 in Register PORTB löschen
-		PORTB &= ~(1 << 6);
-		
-		// Bit 7 in Register PORTB invertieren
-		PORTB ^= (1 << 7);
-		
-		// Bit 2 in Register PORTB invertieren
-		PORTB ^= (1 << 2);
-		
-		PORTB = 0xFF;
-		
-		// Bit 4 in Register PINB abfragen, ob gesetzt
-		if ( (PORTB & (1 << 4)) != 0) {
-			// Bit ist gesetzt
-			PORTB = 0x00;
-		}
-		
-		// Bit 6 in Register PINB abfragen, ob clear
-		if ( (PORTB & (1 << 6)) == 0) {
-			// Bit ist clear
-			PORTB = 0xFF;
-		}
+        if (Taster1_get()) {
+            int x = 0;
+        }
+        if (Taster2_get()) {
+            int x = 0;
+        }
+        if (Taster3_get()) {
+            int x = 0;
+        }
+        if (Taster4_get()) {
+            int x = 0;
+        }
+    }
+    
+    void uebung1() {
+        DDRB = 0xFF;
+        PORTB = 0xAC;
+        
+        //
+        // Uebung
+        //
+        
+        // Bit 1 in Register PORTB setzten
+        PORTB |= (1 << 1);
+        
+        // Bit 3 in Register PORTB setzten
+        PORTB |= (1 << 3);
+        
+        // Bit 5 in Register PORTB setzten
+        PORTB |= (1 << 5);
+        
+        // Bit 2 in Register PORTB löschen
+        PORTB &= ~(1 << 2);
+        
+        // Bit 4 in Register PORTB löschen
+        PORTB &= ~(1 << 4);
+        
+        // Bit 6 in Register PORTB löschen
+        PORTB &= ~(1 << 6);
+        
+        // Bit 7 in Register PORTB invertieren
+        PORTB ^= (1 << 7);
+        
+        // Bit 2 in Register PORTB invertieren
+        PORTB ^= (1 << 2);
+        
+        PORTB = 0xFF;
+        
+        // Bit 4 in Register PINB abfragen, ob gesetzt
+        if ( (PORTB & (1 << 4)) != 0) {
+            // Bit ist gesetzt
+            PORTB = 0x00;
+        }
+        
+        // Bit 6 in Register PINB abfragen, ob clear
+        if ( (PORTB & (1 << 6)) == 0) {
+            // Bit ist clear
+            PORTB = 0xFF;
+        }
     }
 }
 
