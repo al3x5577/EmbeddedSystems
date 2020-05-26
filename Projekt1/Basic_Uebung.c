7e22b966d6eff08d053b425843c37556f291a801
Alex <alexander.modellbau@gmx.de>
2020-05-26 09:39:12 +0200

    Initial Commit

---
+++ Projekt1/Basic_Uebung.c
@@ -0,0 +1,55 @@
+#include <avr/io.h>
+#define F_CPU 1000000
+#include <util/delay.h>
+
+int main(void) {
+    
+    DDRB = 0xFF;
+    PORTB = 0xAB;
+    
+    //
+    // Uebung
+    //
+    
+    // Bit 1 in Register PORTB setzten
+    PORTB |= (1 << 1);
+    
+    // Bit 3 in Register PORTB setzten
+    PORTB |= (1 << 3);
+    
+    // Bit 5 in Register PORTB setzten
+    PORTB |= (1 << 5);
+    
+    // Bit 2 in Register PORTB löschen
+    PORTB &= ~(1 << 2);
+    
+    // Bit 4 in Register PORTB löschen
+    PORTB &= ~(1 << 4);
+    
+    // Bit 6 in Register PORTB löschen
+    PORTB &= ~(1 << 6);
+    
+    // Bit 7 in Register PORTB invertieren
+    PORTB ^= (1 << 7);
+    
+    // Bit 2 in Register PORTB invertieren
+    PORTB ^= (1 << 2);
+    
+    // Bit 4 in Register PINB abfragen, ob gesetzt
+    if ( (PORTB & (1 << 4)) == 1) {
+        // Bit ist gesetzt
+    }
+    
+    // Bit 6 in Register PINB abfragen, ob clear
+    if ( (PORTB & (1 << 6)) == 0) {
+        // Bit ist clear
+    }
+    
+    while (1){
+        // Do some stuff
+        PORTB = 0xFF;
+        _delay_ms (500);
+        PORTB = 0x00;
+        _delay_ms (500);
+    }
+}
---
+++ Projekt1/main.c
@@ -0,0 +1,66 @@
+/*
+ * Projekt1.c
+ *
+ * Created: 26.05.2020 00:20:09
+ * Author : MSR
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 1000000
+#include <util/delay.h>
+
+
+int main(void) {
+	
+	DDRB = 0xFF;
+	
+	
+    /* Replace with your application code */
+    while (1) {
+		
+		PORTB = 0xAC;
+		
+		//
+		// Uebung
+		//
+		
+		// Bit 1 in Register PORTB setzten
+		PORTB |= (1 << 1);
+		
+		// Bit 3 in Register PORTB setzten
+		PORTB |= (1 << 3);
+		
+		// Bit 5 in Register PORTB setzten
+		PORTB |= (1 << 5);
+		
+		// Bit 2 in Register PORTB löschen
+		PORTB &= ~(1 << 2);
+		
+		// Bit 4 in Register PORTB löschen
+		PORTB &= ~(1 << 4);
+		
+		// Bit 6 in Register PORTB löschen
+		PORTB &= ~(1 << 6);
+		
+		// Bit 7 in Register PORTB invertieren
+		PORTB ^= (1 << 7);
+		
+		// Bit 2 in Register PORTB invertieren
+		PORTB ^= (1 << 2);
+		
+		PORTB = 0xFF;
+		
+		// Bit 4 in Register PINB abfragen, ob gesetzt
+		if ( (PORTB & (1 << 4)) != 0) {
+			// Bit ist gesetzt
+			PORTB = 0x00;
+		}
+		
+		// Bit 6 in Register PINB abfragen, ob clear
+		if ( (PORTB & (1 << 6)) == 0) {
+			// Bit ist clear
+			PORTB = 0xFF;
+		}
+    }
+}
+
