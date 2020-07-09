378bbaf23c51bcf36072f977c597b80fe50896ae
Alex <alexander.modellbau@gmx.de>
2020-07-10 00:14:38 +0200

    Commentary

--- EmbeddedSystems/EmbeddedSystems/adc/adc.c
+++ EmbeddedSystems/EmbeddedSystems/adc/adc.c
@@ -26,70 +26,89 @@ void adc_init() {
 }
 
 uint16_t adc_get_LM35() {
-    
     uint32_t avg = 0;
     
+    // Make sum
     for (int i = 0; i < 8; i++) {
         avg += LM35_Array[i];
     }
+    // Make average
     avg = avg / 8;
     
+    // Convert to Degree Celsius
     avg = (int) avg * 0.4883;   // convert to mV, divide by factor of LM35-DZ
     
-    return (avg & 0xffff);  // Return a 16 bit value
+    // Return a 16 bit value
+    return (avg & 0xffff);
 }
 
 uint16_t adc_get_Poti() {
-    
     uint32_t avg = 0;
     
+    // Make sum
     for (int i = 0; i < 8; i++) {
         avg += Poti_Array[i];
     }
-    
+    // Make average
     avg = avg / 8;
     
-    return (avg & 0xffff);  // Return a 16 bit value
+    // Return a 16 bit value
+    return (avg & 0xffff);
 }
 
 ISR(ADC_vect){
+    // Pull data from AD conversion
     uint16_t res = ADC;
+    
 #ifdef DEBUG_LEDS_ADC
     Led3_Off();
 #endif
     
+    // Switch between LM35 and Poti
     switch (ADMUX & (1 << MUX0)) {
-        case 0:
-            if (index_LM35 == 42) { // Trash first conversion
+        case 0: // LM35
+            
+            if (index_LM35 == 42) {
+                // Trash first conversion
                 index_LM35 = 0;
 #ifdef DEBUG_LEDS_ADC
                 Led4_On();
 #endif
             }else if (index_LM35 >= 0 && index_LM35 <= 7) {
+                // Store 8 AD conversions in the Array
                 LM35_Array[index_LM35] = res;
                 index_LM35++;
             }else {
+                // Switch to Poti
                 ADMUX &= ~(30); // Set MUX1..4 to 0
                 ADMUX |= (1 << MUX0);
+                
+                // Set index to default value, so that the first conversion will be trashed
                 index_LM35 = 42;
+                
 #ifdef DEBUG_LEDS_ADC
                 Led4_Off();
 #endif
             }
             break;
             
-        case 1:
+        case 1: // Poti
             if (index_Poti == 42) { // Trash first conversion
 #ifdef DEBUG_LEDS_ADC
                 Led5_On();
 #endif
                 index_Poti = 0;
             }else if (index_Poti >= 0 && index_Poti <= 7) {
+                // Store 8 AD conversions in the Array
                 Poti_Array[index_Poti] = res;
                 index_Poti++;
             }else {
+                // Switch to LM35
                 ADMUX &= ~(31); // Set MUX0..4 to 0
+                
+                // Set index to default value, so that the first conversion will be trashed
                 index_Poti = 42;
+                
 #ifdef DEBUG_LEDS_ADC
                 Led5_Off();
 #endif
@@ -102,5 +121,7 @@ ISR(ADC_vect){
 #ifdef DEBUG_LEDS_ADC
     Led3_On();
 #endif
+    
+    // Trigger next conversion
     ADCSRA |= (1 << ADSC) | (1 << ADIE);
 }
