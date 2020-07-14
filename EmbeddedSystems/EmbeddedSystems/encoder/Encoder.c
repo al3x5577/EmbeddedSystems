4fac47d3f4c2dde14ac39ec2981fb23cf1cc19d5
Alex <alexander.modellbau@gmx.de>
2020-07-14 14:35:25 +0200

    New Encoder
    Encoder with state machine

--- EmbeddedSystems/EmbeddedSystems/encoder/Encoder.c
+++ EmbeddedSystems/EmbeddedSystems/encoder/Encoder.c
@@ -1,69 +1,154 @@
 
 #include "Encoder.h"
 
-volatile int8_t enc_delta;          // -128 ... 127
-static int8_t last;
+volatile uint8_t enc_state = 0;
+volatile uint8_t st_m_state = 0;
+volatile uint8_t last = 0;
+volatile uint8_t new = 0;
 
-void encoder_isr(){     // 1ms for manual movement
-  int8_t new, diff;
+volatile int16_t val = 0;
 
-  new = 0;
-  if( PHASE_A ) new = 3;
-  if( PHASE_B ) new ^= 1;          // convert gray to binary
-  diff = last - new;               // difference last - new
-  if( diff & 1 ) {                 // bit 0 = value (1)
-    last = new;                    // store new as next last
-    enc_delta += (diff & 2) - 1;   // bit 1 = direction (+/-)
-  }
+/**
+ State machine for encoder
+ */
+void encoder_process() {
+    switch (st_m_state) {
+        case 1: // Init state
+            
+            if (enc_state == 1) {
+                // Rotation forward
+                st_m_state = 2;
+                
+            } else if (enc_state == 2) {
+                // Rotation backward
+                st_m_state = 5;
+            }
+            break;
+            
+        case 2: // Rotation forward
+            
+            if (enc_state == 3) {
+                // Still rotating forward
+                st_m_state = 3;
+                
+            } else if (enc_state == 0) {
+                // Fail, go back to init state (maybe bouncing)
+                st_m_state = 1;
+            }
+            
+            break;
+            
+        case 3: // Rotation forward
+        
+            if (enc_state == 2) {
+                // Still rotating forward
+                st_m_state = 4;
+                
+            } else if (enc_state == 1) {
+                // Fail, go back to previous state (maybe bouncing)
+                st_m_state = 2;
+            }
+            break;
+            
+        case 4: // Rotation forward
+        
+            if (enc_state == 0) {
+                // Forward Rotation done
+                st_m_state = 1;
+                val++;
+                
+            } else if (enc_state == 3) {
+                // Fail, go back to previous state (maybe bouncing)
+                st_m_state = 3;
+            }
+            break;
+            
+            
+        case 5: // Rotation backward
+        
+            if (enc_state == 0) {
+                // Fail, go back to init state (maybe bouncing)
+                st_m_state = 1;
+                
+            } else if (enc_state == 3) {
+                // Still rotating backward
+                st_m_state = 6;
+            }
+            break;
+            
+        case 6: // Rotation backward
+        
+            if (enc_state == 2) {
+                // Fail, go back to previous state (maybe bouncing)
+                st_m_state = 5;
+                
+            } else if (enc_state == 1) {
+                // Still rotating backward
+                st_m_state = 7;
+            }
+            break;
+            
+        case 7: // Rotation backward
+        
+            if (enc_state == 3) {
+                // Fail, go back to previous state (maybe bouncing)
+                st_m_state = 6;
+                
+            } else if (enc_state == 0) {
+                // Rotating backward done
+                st_m_state = 1;
+                val--;
+            }
+            break;
+
+            
+        default:
+            st_m_state = 1;
+            break;
+    }
+}
+
+void encoder_isr(){
+    
+    // Update enc_state
+    
+    // If data pin 1 of encoder is HIGH, set val to bX1
+    if (PHASE_A) {
+        enc_state |= (1 << 0);
+    }else{  // If data pin 1 of encoder is LOW, set val to bX0
+        enc_state &= ~(1 << 0);
+    }
+    // If data pin 2 of encoder is HIGH, set val to b1X
+    if (PHASE_B) {
+        enc_state |= (1 << 1);
+    }else{  // If data pin 2 of encoder is HIGH, set val to b0X
+        enc_state &= ~(1 << 1);
+    }
+    new = enc_state;
+    
+    
+    // If nothing changed, keep state
+    if (last == new) {
+        return;
+    }
+    
+    // State machine
+    encoder_process();
+    
 }
 
 void encode_init( void ){
-    Timer2_init(16, encoder_isr); // Init timer
-  int8_t new;
-
-  new = 0;
-  if( PHASE_A ) new = 3;
-  if( PHASE_B ) new ^= 1;       // convert gray to binary
-  last = new;                   // power on state
-  enc_delta = 0;
+    
+    // Init state machine
+    st_m_state = 1;
+    
+    // Init timer (16MHZ, call encoder_isr() at timer isr)
+    Timer2_init(16, encoder_isr);
 }
 
-
-
-int8_t encode_read2( void )         // read two step encoders
-{
-  int8_t val;
-
-  cli();
-  val = enc_delta;
-  enc_delta = val & 1;
-  sei();
-  return val >> 1;
-}
-
-
-
-int loop( void )
-{
-  int32_t val = 0;
-
-  encode_init();
-  sei();
-
-  for(;;){
-    val += encode_read2();          // read a single step encoder
-      
-      char str[30];
-      sprintf(str, "val: %d\n", val);
-      uart_send(str);
-      
-    // shift as many ones as count to byte
-    int ledByte = 0;
-    for (int i = 0; i < val; i++) {
-        ledByte = ledByte << 1;
-        ledByte++;
-    }
-    // set Port
-    PORTB = ledByte;
-  }
+int16_t encoder_get(){
+    TIMSK2 &= ~(1 << OCIE0A);
+    int16_t temp = val;
+    TIMSK2 |= (1 << OCIE0A);
+    return temp;
 }
--- EmbeddedSystems/EmbeddedSystems/encoder/Encoder.h
+++ EmbeddedSystems/EmbeddedSystems/encoder/Encoder.h
@@ -10,17 +10,27 @@
 #ifndef MY_ENCODER_H_
 #define MY_ENCODER_H_
 
+#define DEBUG_MY_ENCODER_
+
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include "../timer/Timer.h"
+
+// Debug
+#ifdef DEBUG_MY_ENCODER_
 #include "../uart/Uart.h"
 #include <string.h>
+#endif
+
 
 #define PHASE_A     (PINC & 1<<PC6)
 #define PHASE_B     (PINC & 1<<PC7)
 
+#define STATE_0     (enc_state & (1<<0))
+#define STATE_1     (enc_state & (1<<1))
+
 void encode_init(void);
 
-int loop(void);
+int16_t encoder_get(void);
+
 
 #endif /* MY_ENCODER_H_ */
