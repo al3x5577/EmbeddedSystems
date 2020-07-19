fc49aac05bbce214c4f20e671c406c9ff38557ce
Alex <alexander.modellbau@gmx.de>
2020-07-19 20:04:35 +0200

    Update main.c

--- EmbeddedSystems/EmbeddedSystems/main.c
+++ EmbeddedSystems/EmbeddedSystems/main.c
@@ -50,7 +50,7 @@ int main(void) {
         if((Timer_getTick() - timeVarMain) >= 500){
             timeVarMain = Timer_getTick();
             
-            PORTB = PORTB ^ 0xff;
+            //PORTB = PORTB ^ 0xff;
             
             /*if ( isafd == 0){
                 Led1_On();
